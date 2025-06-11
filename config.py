d57f3bf21752ca4ad06f8dd8074a0bc0285894af
Daniel Alejandro <d.toro05@ufromail.cl>
2025-06-11 13:41:51 -0400

    fix : config

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
 import os
 
+
 class Settings():
 
     SUPABASE_URL: str = os.getenv("SUPABASE_URL")
-    SUPABASE_KEY: str = os.getenv("SUPABASE_KEY") 
+    SUPABASE_KEY: str = os.getenv("SUPABASE_KEY")
     SUPABASE_SERVICE_KEY: str = os.getenv("SUPABASE_SERVICE_KEY")
     TOKEN_EXPIRY = os.getenv("TOKEN_EXPIRY")
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET")
 
     PROJECT_NAME: str = "FirmiFy"
 
