662d33980b7a2236d17a152a95a8001dd2009c11
seb <s.vidal06@ufromail.cl>
2025-06-15 22:51:25 -0400

    fix: fixed project config; feat: imported auth_service.get_user_id() on request_database.py

--- src/database/request_database.py
+++ src/database/request_database.py
@@ -1,11 +1,11 @@
 from ..database.deps import super_client
 from ..models.request_models.request_in import RequestRequest, RequestSave
+from ..service import auth_service
 import datetime
 
 from fastapi import HTTPException
 
 def create_request(request: RequestSave):
-    # user = logged_in_user
 
     request = super_client.table("request").insert({
         "id": (int(super_client.from("request").select('*', {
@@ -14,9 +14,9 @@ def create_request(request: RequestSave):
         "end_date": request.end_date,
         "file": request.file,
         "category_id": request.category_id,
-        # "user_id": user.id,
-        "state_id": request.state_id
-    })
+        "user_id": auth_service.get_user_id(),
+        "state_id": 1
+    }).execute()
 
 def get_request_from_date_intervals(from_date: datetime, until_date: datetime):
     request = super_client.table("request").select("start_date")
